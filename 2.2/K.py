4f9278755de0d2334bf78a17bc1a73f80b6a4463
it-12 <stud@it-12>
2023-09-23 09:38:44 +0300

    fixed bad code 2.2/K.py

--- 2.2/K.py
+++ 2.2/K.py
@@ -1,35 +1,11 @@
-def solution(number: int):
-    fst = number // 100
-    snd = number // 10 % 10
-    lst = number % 10
-    mx = 0
-    md = 0
-    mn = 0
-
-    if fst > snd and fst > lst:
-        mx = fst
-        if snd > lst:
-            md = snd
-            mn = lst
-        else:
-            md = lst
-            mn = snd
-    elif snd > fst and snd > lst:
-        mx = snd
-        if fst > lst:
-            md = fst
-            mn = lst
-        else:
-            md = lst
-            mn = fst
-    elif fst > snd:
-        mx = lst
-        md = fst
-        mn = snd
-    else:
-        mx = lst
-        md = snd
-        mn = fst
+def solution(number: str):
+    digits = list(map(int, number))
+    mx = max(digits)
+    mn = min(digits)
+    digits.remove(mx)
+    digits.remove(mn)
+    md = digits[0]
+    
         
     if (mx + mn) == md * 2:
         print('YES')
@@ -38,9 +14,10 @@ def solution(number: int):
 
 
 def main():
-    number = int(input())
+    number = input()
     solution(number)
 
 
 if __name__ == '__main__':
     main()
+
